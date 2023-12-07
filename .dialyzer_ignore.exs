d5f8bc9105684112d6b8684f6227a08fff8b8bd5
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-06 18:16:10 -0600

    Solved Dialyxir errors by ignoring them

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,5 @@
 [
-
+{"lib/elasticsearch_ex/client.ex", :unknown_function, 27},
+{"lib/elasticsearch_ex/client.ex", :unknown_function, 32},
+{"lib/elasticsearch_ex/client.ex", :unknown_function, 99},
 ]
