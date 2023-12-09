7732a93999fa16128f915e60a5e52cc6bb5e550f
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-08 18:32:31 -0600

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule ElasticsearchEx.MixProject do
   defp dialyzer do
     [
       plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
-      plt_add_apps: [:any_http, :jason],
+      plt_add_apps: [],
       list_unused_filters: true
     ]
   end
