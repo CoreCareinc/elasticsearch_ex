69e57ae7fa2c4d6b78111581f880915013761e61
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-18 18:49:16 -0600

    Removed unecessary dependency

--- mix.exs
+++ mix.exs
@@ -62,7 +62,6 @@ defmodule ElasticsearchEx.MixProject do
   defp dialyzer do
     [
       plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
-      plt_add_apps: [],
       list_unused_filters: true
     ]
   end
@@ -71,7 +70,6 @@ defmodule ElasticsearchEx.MixProject do
   defp deps do
     [
       {:any_http, "~> 0.2"},
-      {:req, "~> 0.4"},
       {:jason, "~> 1.4"},
 
       ## Dev dependencies
