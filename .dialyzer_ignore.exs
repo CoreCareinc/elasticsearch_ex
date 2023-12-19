e3591755d883b6d65f4d247f70ae50e5a3442d69
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-19 07:55:41 -0600

    Fixe Dialyzer errors

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,8 @@
 [
+  {"lib/elasticsearch_ex/client.ex", :unknown_function, 129},
   {"lib/elasticsearch_ex/client.ex", :unknown_function, 26},
   {"lib/elasticsearch_ex/client.ex", :unknown_function, 87},
-  {"lib/elasticsearch_ex/client.ex", :unknown_function, 129},
-  {"lib/elasticsearch_ex/error.ex", :unknown_type, 22}
+  {"lib/elasticsearch_ex/error.ex", :unknown_type, 22},
+  {"lib/elasticsearch_ex/ndjson.ex", :unknown_function, 24},
+  {"lib/elasticsearch_ex/ndjson.ex", :unknown_function, 41}
 ]
