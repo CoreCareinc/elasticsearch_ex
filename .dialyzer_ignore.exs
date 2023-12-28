65a8e755b9b61b8e98885926ea983539a44ac20a
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-28 11:53:17 -0600

    Disabled Dialyzer ignore file

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,8 +1,1 @@
-[
-  {"lib/elasticsearch_ex/client.ex", :unknown_function, 27},
-  {"lib/elasticsearch_ex/client.ex", :unknown_function, 82},
-  {"lib/elasticsearch_ex/client.ex", :unknown_function, 140},
-  {"lib/elasticsearch_ex/error.ex", :unknown_type, 22},
-  {"lib/elasticsearch_ex/ndjson.ex", :unknown_function, 24},
-  {"lib/elasticsearch_ex/ndjson.ex", :unknown_function, 41}
-]
+[]
