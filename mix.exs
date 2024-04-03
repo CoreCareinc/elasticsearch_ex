e87c08de66099cac67165a88e4413ac9237470fc
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2024-04-03 13:12:10 -0600

    Updated dependencies
    New:
      castore 1.0.6
      finch 0.18.0
      hpax 0.1.2
      mint 1.5.2
      nimble_options 1.1.0
      nimble_ownership 0.3.1
      nimble_pool 1.1.0
      req 0.4.14
    Upgraded:
      cowboy 2.10.0 => 2.12.0
      cowlib 2.12.1 => 2.13.0
      plug_cowboy 2.7.0 => 2.7.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ElasticsearchEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/CoreCareinc/elasticsearch_ex"
-  @version "1.3.0"
+  @version "1.3.1"
 
   def project do
     [
@@ -77,7 +77,7 @@ defmodule ElasticsearchEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:any_http, "~> 0.6"},
+      {:req, "~> 0.4"},
       {:jason, "~> 1.4"},
 
       ## Dev dependencies
